bdb66d4fa29e4133543fac1adb79c03144bad0bc
FearTheVenator <mordecai556@gmail.com>
2024-01-27 22:28:30 -0600

    Cop works now god

--- Cop.gd
+++ Cop.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 var tar_rot : float
 var chasing_player:bool = false
-var player: Node3D
+var player: CharacterBody3D
 func _ready():
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
@@ -13,7 +13,6 @@ func _ready():
 	navigation_agent.target_desired_distance = 0.5
 	# Make sure to not await during _ready.
 	call_deferred("actor_setup")
-
 func actor_setup():
 	# Wait for the first physics frame so the NavigationServer can sync.
 	await get_tree().physics_frame
@@ -26,7 +25,8 @@ func set_movement_target(movement_target: Vector3):
 
 func _physics_process(delta):
 	if chasing_player == true:
-		navigation_agent.set_target_position(player.get_position())
+		print(player.get_global_position())
+		navigation_agent.set_target_position(player.get_global_position())
 	elif navigation_agent.is_navigation_finished():
 		movement_target_position = Vector3(randf_range(-100,100),0.0,randf_range(-100,100))
 		navigation_agent.set_target_position(movement_target_position)
@@ -41,17 +41,17 @@ func _physics_process(delta):
 	#var cop_dir = next_path_position - current_agent_position
 	tar_rot = atan2(velocity.x, velocity.z)
 	rotation.y = lerp_angle(rotation.y, tar_rot,0.1)
+	
 	move_and_slide()
 
 
-#func _on_character_detector_body_entered(body):
-	#print(body)
-	#if body.name == "player":
-		#chasing_player = true
-		#player = body.get_parent()
-		#navigation_agent.set_target_position(body.position)
-	#else:
-		#while !navigation_agent.is_target_reachable():
-			#movement_target_position = Vector3(randf_range(-100,100),0.0,randf_range(-100,100))
-			#navigation_agent.set_target_position(movement_target_position)
+func _on_character_detector_body_entered(body):
+	print(body)
+	if body.name == "player":
+		chasing_player = true
+		player = body.get_parent().get_child(0)
+		print(player)
+		navigation_agent.set_target_position(player.get_global_position())
+	else:
+		pass
 		
