e9c4af3b56dd8b4f62ca6a7b787efaad6f8a5017
FearTheVenator <mordecai556@gmail.com>
2024-01-27 21:48:31 -0600

    oh look a sorta of broken ai

--- Cop.gd
+++ Cop.gd
@@ -4,12 +4,13 @@ extends CharacterBody3D
 @export var movement_target_position: Vector3 = Vector3(0.5,0.0,-3)
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 var tar_rot : float
+var chasing_player:bool = false
+var player: Node3D
 func _ready():
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
 	navigation_agent.path_desired_distance = 0.5
 	navigation_agent.target_desired_distance = 0.5
-
 	# Make sure to not await during _ready.
 	call_deferred("actor_setup")
 
@@ -24,7 +25,9 @@ func set_movement_target(movement_target: Vector3):
 	navigation_agent.set_target_position(movement_target)
 
 func _physics_process(delta):
-	if navigation_agent.is_navigation_finished():
+	if chasing_player == true:
+		navigation_agent.set_target_position(player.get_position())
+	elif navigation_agent.is_navigation_finished():
 		movement_target_position = Vector3(randf_range(-100,100),0.0,randf_range(-100,100))
 		navigation_agent.set_target_position(movement_target_position)
 		while !navigation_agent.is_target_reachable():
@@ -41,12 +44,14 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-func _on_character_detector_body_entered(body):
-	print(body)
-	if body.name == "player":
-		navigation_agent.set_target_position(body.position)
-	else:
-		while !navigation_agent.is_target_reachable():
-			movement_target_position = Vector3(randf_range(-100,100),0.0,randf_range(-100,100))
-			navigation_agent.set_target_position(movement_target_position)
+#func _on_character_detector_body_entered(body):
+	#print(body)
+	#if body.name == "player":
+		#chasing_player = true
+		#player = body.get_parent()
+		#navigation_agent.set_target_position(body.position)
+	#else:
+		#while !navigation_agent.is_target_reachable():
+			#movement_target_position = Vector3(randf_range(-100,100),0.0,randf_range(-100,100))
+			#navigation_agent.set_target_position(movement_target_position)
 		
